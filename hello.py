85560d4f03d81f4d6ffd7190e5820816b829d9ad
Polaris Yu <polaris.yu@EF.com>
2018-05-31 18:10:19 +0800

    Try to get user input

--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", method=["GET", "POST"])
 def source():
 	html = 'Hello World!'
 	return html
+
+def application():
+	answer = logic("Example Input")
+	return answer
+
+if __name__ == "__main__":
+	app.run()
