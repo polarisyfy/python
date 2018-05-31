a69ca9ed2a2be03bd936702c1206cd569e539929
Polaris Yu <polaris.yu@EF.com>
2018-05-31 17:49:31 +0800

    Change the start.py file, the input command cannot be compiled successfully

--- start.py
+++ start.py
@@ -2,20 +2,24 @@ from GetCentersByBU import xlsxHandler
 from GetDestructSharingRules import generateDestructByBU
 from CreateXML import generateXML
 
-rootPath = input("1/5. Input origin sharingRules folder path: ")
+# rootPath = input("1/5. Input origin sharingRules folder path: ")
+rootPath = "C:\\"
 rootPath.replace("\\", "/")
 
-listObjs = input("2/5. Input the objects: ").replace(" ", "").split(",")
+# listObjs = input("2/5. Input the objects: ").replace(" ", "").split(",")
+listObjs = ["Account", "Lead"]
 
 listCenters = []
 dictCenters = {}
-wbPath = input("3/5. Input the excel file path for center list: ")
+# wbPath = input("3/5. Input the excel file path for center list: ")
+wbPath = "C:\\CenterList.xlsx"
 gotoNext = "Y"
 while gotoNext == "Y":
-	targetBU = input ("4/5. Input the targetBU: ")
+	# targetBU = input ("4/5. Input the targetBU: ")
+	targetBU = "China Own"
 	(listCenters, dictCenters) = xlsxHandler(wbPath, targetBU, listCenters, dictCenters)
 	#generateDestructByBU(rootPath, listObjs, listCenters)
 	generateXML(rootPath, targetBU, dictCenters, listObjs)
-	gotoNext = input("Has next BU? (Y/N):")
+	# gotoNext = input("Has next BU? (Y/N):")
 
 print ("5/5 Done!")
